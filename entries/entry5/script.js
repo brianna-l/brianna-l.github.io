a74c8803f6b8a6e024373d360b1053254bc0622d
brianna-l <loub697@newschool.edu>
2023-10-17 20:45:50 -0400

    initial post

---
+++ entries/entry5/script.js
@@ -0,0 +1,9 @@
+document.getElementById('backgroundSwitcher').addEventListener('change', function() {
+    if(this.checked) {
+        document.body.style.backgroundColor = 'black';
+        document.body.style.color = 'white';
+    } else {
+        document.body.style.backgroundColor = 'white';
+        document.body.style.color = 'black';
+    }
+});
