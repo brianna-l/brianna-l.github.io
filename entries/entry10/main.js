cce77c5fb340ed8ed0245cdfc36b24f6a08f1ebc
brianna-l <loub697@newschool.edu>
2023-12-04 01:12:28 -0500

    entry10

--- entries/entry10/main.js
+++ entries/entry10/main.js
@@ -1,13 +1,3 @@
-
-let nav = document.querySelector(".nav");
-nav.addEventListener("click", normal)
-
-function normal() {
-    nav.classList.toggle("normal");
-    console.log("sink");
-}
-
-
 let body = document.body;
 
 function turnOnLight() {
@@ -18,3 +8,28 @@ function turnOnLight() {
   
   let button = document.querySelector(".button");
   button.addEventListener("click", turnOnLight);
+
+//
+
+let X = Y = 0;
+const Light = document.getElementById('flashlight')
+document.addEventListener('mousemove',(e)=>{
+    X = e.clientX
+    Y = e.clientY
+    Light.style.setProperty('--x',`${X}px`)
+    Light.style.setProperty('--y',`${Y}px`)
+})
+
+const [min,max] = [5,20]
+document.addEventListener('keyup',(e)=>{
+    let R = Light.style.getPropertyValue('--r').replace(/\D/g,'')
+    if(e.key == '[' && R > min){
+        Light.style.setProperty('--r',`${--R}em`)
+    }
+    if(e.key == ']' && R < max){
+        Light.style.setProperty('--r',`${++R}em`)
+    }
+    if(e.key === 'l'){
+        Light.style.setProperty('--r',`-${min-1}`)
+    }
+})
